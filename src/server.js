4d87e7df5246abcecae1b97fb8c34a70ec134465
Tyler Weng <tylercweng@gmail.com>
2017-12-27 15:00:05 -0800

    Initialize project structure

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import express from 'express'
+import app from './app'
+import serveFavicon from 'serve-favicon'
+
+const staticPath = path.resolve(__dirname, '../static')
+
+// Initialize express server
+export default function(callback) {
+  const server = express()
+
+  // Application-level environment vars
+  server.set('env', process.env.NODE_ENV || 'development')
+  server.set('host', process.env.HOST || '0.0.0.0')
+  server.set('port', process.env.PORT || 80)
+
+  // Application-level middleware
+  server.use(serveFavicon(`${staticPath}/assets/favicon.ico`))
+  server.use((err, req, res, next) => {
+    console.log('Error on request %s %s', req.method, req.url)
+    console.log(err)
+    console.log(err.stack)
+    res.status(500).send('Uncaught server error')
+  })
+
+  return server.listen(server.get('port', () => callback(server)))
+}
