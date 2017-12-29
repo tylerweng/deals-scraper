34d964587089e5d5527b891849e643e0435389d7
Tyler Weng <tylercweng@gmail.com>
2017-12-29 09:42:37 -0800

    Deletes unnecessary js suffix

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-import config from './config.js'
+import config from './config'
 
 // Using mongolab url for now
 const database_url = `mongodb://${config.username}:${config.password}@${config.url}`
